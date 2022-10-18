926e47b9172cef02459ee04cd2368824a5b777cb
Henry W <henry.woodmansee@gmail.com>
2022-10-17 17:03:36 -0700

    commit

--- week-4/d1-basic-coding-principles/practices/01-reading-tests-exercise/test/specs.js
+++ week-4/d1-basic-coding-principles/practices/01-reading-tests-exercise/test/specs.js
@@ -3,18 +3,18 @@ const expect = chai.expect;
 const { isFive, isOdd, myRange } = require("../funcs");
 
 describe("isFive(num)", () => {
-  it("should return true if the num is 5 otherwise false", () => {
+  it("should return true if the num is 5 otherwise false", () => { // testing to see if exported function 'isFive' returns a boolean value of whether or not the argument given === 5
     const num1 = 5;
 
-    const actual1 = isFive(num1); 
+    const actual1 = isFive(num1);
 
     expect(actual1).to.be.true;
 
     const otherInput = "not 5";
     const num2 = 4;
 
-    const actual2 = isFive(num2); 
-    const actual3 = isFive(otherInput); 
+    const actual2 = isFive(num2);
+    const actual3 = isFive(otherInput);
 
     expect(actual2).to.be.false;
     expect(actual3).to.be.false;
@@ -22,35 +22,35 @@ describe("isFive(num)", () => {
 });
 
 describe("isOdd(number)", () => {
-  it("should return true if number is odd", () => {
+  it("should return true if number is odd", () => { // testing for function 'isOdd' to return true if argument is odd
     const num1 = 3;
     const num2 = 2953;
     const num3 = -999;
 
-    const actual1 = isOdd(num1); 
-    const actual2 = isOdd(num2); 
-    const actual3 = isOdd(num3); 
+    const actual1 = isOdd(num1);
+    const actual2 = isOdd(num2);
+    const actual3 = isOdd(num3);
 
     expect(actual1).to.be.true;
     expect(actual2).to.be.true;
     expect(actual3).to.be.true;
   });
 
-  it("should return false if the num is even", () => {
+  it("should return false if the num is even", () => { // same but false if argument is even
     const num1 = 4;
     const num2 = 2952;
     const num3 = -998;
 
-    const actual1 = isOdd(num1); 
-    const actual2 = isOdd(num2); 
-    const actual3 = isOdd(num3); 
+    const actual1 = isOdd(num1);
+    const actual2 = isOdd(num2);
+    const actual3 = isOdd(num3);
 
     expect(actual1).to.be.false;
     expect(actual2).to.be.false;
     expect(actual3).to.be.false;
   });
 
-  it("should throw an error if num is not type of Number", () => {
+  it("should throw an error if num is not type of Number", () => { // testing for function 'isOdd' to return an error if type of argument is anything other than number
     const string = "i am a string";
     const object = { i: "am", an: "object" };
     const array = ["i", "am", "an", "array"];
@@ -63,7 +63,7 @@ describe("isOdd(number)", () => {
 
 describe("myRange(min, max, step)", () => {
   context("if step is not provided", () => {
-    it("should return the correct array with default value step=1", () => {
+    it("should return the correct array with default value step=1", () => { // testing for function 'myRange' to return the correct array with given default step value
       const [min1, max1] = [0, 5];
       const [min2, max2] = [6, 3];
 
@@ -78,7 +78,7 @@ describe("myRange(min, max, step)", () => {
   });
 
   context("if step is provided", () => {
-    it("should return the correct array", () => {
+    it("should return the correct array", () => { // testing for function 'myRange' to return the correct array without given default value
       const [min1, max1, step1] = [0, 5, 1];
       const [min2, max2, step2] = [0, 5, 2];
       const [min3, max3, step3] = [9, 5, 2];
