35bf9a8bd975ba86e78e0fa4ca399a3761487262
Henry W <henry.woodmansee@gmail.com>
2022-10-19 09:02:53 -0700

    context notes

--- week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
+++ week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
@@ -1,5 +1,5 @@
 function eat() {
-  console.log(`${this.name} eats fish food`);
+  console.log(`${this.name} eats fish food`); // undefined
 }
 
 class Fish {
@@ -13,25 +13,26 @@ class Fish {
 }
 
 const nemo = new Fish('Nemo');
+const dory = new Fish('Dory')
+
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+// eat(); // ? undefined eats fish food
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
-
+// nemo.eat(); // Nemo eats fish food
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
+// eat(); // undefined eats fish food
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+// nemo.swim(); // Nemo swimming in the water
 
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
-// swim(); // ?
+// swim() // TypeError: Cannot read properties of undefined (reading 'name')
